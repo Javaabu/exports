b3b971d0ff6b944a0f7bba9b4924174d814c5430
Arushad Ahmed <dash-8x@hotmail.com>
2024-08-20 22:08:29 +0500

    - Fixed phpunit

---
+++ tests/TestSupport/Factories/UserFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Javaabu\Exports\Tests\TestSupport\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Javaabu\Exports\Tests\TestSupport\Models\User;
+
+class UserFactory extends Factory
+{
+    protected $model = User::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name,
+            'email' => fake()->unique()->safeEmail,
+        ];
+    }
+}
---
+++ tests/TestSupport/Models/User.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Javaabu\Exports\Tests\TestSupport\Models;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Javaabu\Stats\Tests\TestSupport\Factories\UserFactory;
+
+class User extends Authenticatable
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected static function newFactory()
+    {
+        return new UserFactory();
+    }
+
+    public function scopeSearch($query, $search)
+    {
+        return $query->where('name', 'like', '%' . $search . '%');
+    }
+
+    public function getMorphClass()
+    {
+        return 'user';
+    }
+}
